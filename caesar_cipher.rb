3f7c58baa3bb0727c6853688ce478dcaf8df9a62
Anais Corinne Engelmajer <anaiscorinne@gmail.com>
2015-12-17 14:03:41 -0500

    Iteration 1 complete

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,10 +1,11 @@
 def solve_cipher(input)
+  input_arr = Array.new(input.chars)
   decoded_message = ""
-  input_arr = Array.new(input.chars.to_a)
   input_arr.each do | char | 
       shifted_char = (char.ord - 1).chr 
-  puts decoded_message + shifted_char
+  decoded_message += shifted_char
   end
+  puts decoded_message
 end
 
 solve_cipher("ifmmp")
