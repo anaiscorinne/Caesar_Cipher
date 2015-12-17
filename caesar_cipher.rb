004fb525eed34c0c7650d50638b85b404341ced4
Anais Corinne Engelmajer <anaiscorinne@gmail.com>
2015-12-17 12:50:45 -0500

    Now decoded but printing characters on separate lines

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,10 @@
 def solve_cipher(input)
+  decoded_message = ""
   input_arr = Array.new(input.chars.to_a)
+  input_arr.each do | char | 
+      shifted_char = (char.ord - 1).chr 
+  puts decoded_message + shifted_char
+  end
 end
 
 solve_cipher("ifmmp")
-# should return "hello"
