be4ca456a0d5eb5825d3d944bffcb3d3594bbe79
Anais Corinne Engelmajer <anaiscorinne@gmail.com>
2015-12-28 12:48:34 -0500

    added ability to tell function how many shifts to make, still need to figure out spaces and how to create my own alphabet

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,11 +1,11 @@
-def solve_cipher(input)
+def solve_cipher(input, x)
   input_arr = Array.new(input.chars)
   decoded_message = ""
   input_arr.each do | char | 
-      shifted_char = (char.ord - 1).chr 
+      shifted_char = (char.ord + x).chr
   decoded_message += shifted_char
   end
   puts decoded_message
 end
 
-solve_cipher("ifmmp")
+solve_cipher("pb uhdo qdph lv grqdog gxfn", -3)
